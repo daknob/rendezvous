c287cf2710e1a4342532b9373ef80b85da4ded77
Antonios Chariton (daknob) <daknob.mac@gmail.com>
2021-10-25 01:26:41 +0200

    Convert about.php to SQLite3
    This commit also includes some drive-by improvements to the `include()`
    statements as well as the project link on GitHub.

--- about.php
+++ about.php
@@ -2,19 +2,19 @@
 session_start();
 session_save_path(DB_DIR);
 
-include("db.php");     // include txtDB
-include("conf.php");   // settings
+include("db.php");     // Include the database
+include("conf.php");   // Settings
 
 include("header.inc.php");
-include "php/show_links.php";
-include("https_check.inc.php");  // check for https and redirect if necessary
+include("php/show_links.php");
+include("https_check.inc.php");  // Use HTTPS exclusively
 
 ?>
 
 <br/>
 To get a copy or contribute to the development of the project visit
 <a href="https://github.com/zakkak/rendezvous">
-  https://github.com/zakkak/rendezvous
+  it on GitHub
 </a>.
 <br/>
 <br/>
